768ce68654324f30c18e1e2c544dec24f5e2127b
Anandkumar <anandkumar316sw@gmail.com>
2018-07-09 17:03:55 +0530

    Assignment-1

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,13 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (str) => {
+  const splitString = str.split('');
+
+  const reverseArray = splitString.reverse();
+
+  const joinArray = reverseArray.join('');
+
+  return joinArray;
+};
 
 module.exports = reverse;
